6177e6d76f1809d563792d8df8da4a90248dc185
Gp9610 <80778847+Gp9610@users.noreply.github.com>
2021-05-13 11:59:05 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -20,4 +20,20 @@ albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
 
 #non usiamo lo schema RGB perchè abbiamo un singolo strato
 #useremo una colorRampPalette, e decideremo i colori da utilizzare
-ColorRampPalette
+cl <- colorRampPalette(c("lightblue","green","red","yellow")) (100)
+plot(albedo,col=cl)
+#ricampionare il dataset
+#per aggregare i nostri pixel-> aggregate: immagine con i pixel
+#maggiore è il numero dei pixel maggiore è il peso dell'immagine
+#prendo un pixel più grande ed estraggo la media dei valori all' interno-> 
+# trasformo la mia immagine di n pixel a un nemro inferiore di pixel
+#fattore "fact" diminuiso linermente i pixel (100x100pixel=1pixel)
+#è un ricampiomento bi-lineare
+albedores <- aggregate(albedo, fact=100)
+#il numero di celle rispetto a prima è diminuito e molto più veloce da visualizzare
+plot(albedores, col=cl)
+
+#install.packages("knitr")
+#install.packages("RStoolbox")
+#library(knitr)
+#library(RStoolbox)
